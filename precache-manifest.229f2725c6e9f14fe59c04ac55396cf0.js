0f0dc3c645e7d85871bb00914498ce1545f07419
bdbose <bdbose123@gmail.com>
2021-05-14 01:06:27 +0530

    Updates

---
+++ precache-manifest.229f2725c6e9f14fe59c04ac55396cf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc57fdd0b6353de521cfd3f69c8150a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6be69aa7dbe60aa1081",
+    "url": "./static/css/main.540028bd.chunk.css"
+  },
+  {
+    "revision": "ecd3e4777e4086cf46b9",
+    "url": "./static/js/2.aae978d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.aae978d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6be69aa7dbe60aa1081",
+    "url": "./static/js/main.d2c37759.chunk.js"
+  },
+  {
+    "revision": "820265918b8fd2c78bec",
+    "url": "./static/js/runtime-main.ab3389a6.js"
+  },
+  {
+    "revision": "932a22ab9bda1d870194831526b947b9",
+    "url": "./static/media/tinder.932a22ab.svg"
+  }
+]);
